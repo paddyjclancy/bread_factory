4dcf195fc34dd7b20cb22aeb65aa43cbb0f9d9a5
paddyjclancy <patrickclancy0605@gmail.com>
2020-05-28 17:20:10 +0100

    Created defs and tests for making and baking

--- factory_functions_def.py
+++ factory_functions_def.py
@@ -1,8 +1,29 @@
 
 def make_dough(ing1, ing2):
     if ing1 == "water" and ing2 == "flour":
-        return "Dough"
+        return "dough"
     elif ing1 == "flour" and ing2 == "water":
-        return "Dough"
+        return "dough"
     else:
-        return "Not dough"
+        return "not dough"
+
+def bake_bread(ing):
+    if ing == "dough":
+        return "bread"
+    else:
+        return "not bread"
+
+def make_ww_dough(ing1, ing2):
+    if ing1 == "whole wheat flour" and ing2 =="water":
+        return "whole wheat dough"
+    elif ing1 == "water" and ing2 == "whole wheat flour":
+        return "whole wheat dough"
+    else:
+        return "not whole wheat dough"
+
+def bake_ww_bread(ing):
+    if ing == "whole wheat dough":
+        return "whole wheat bread"
+    else:
+        return "not whole wheat bread"
+
--- factory_functions_test.py
+++ factory_functions_test.py
@@ -1,14 +1,40 @@
 from factory_functions_def import *
 
-# Test 1
+### PART 1 - Regular dough / bread ###
+# Test 1  -  make_dough()
 
-print("When make_dough is called with 'water' and 'flour' it should return 'Dough'")
-print(make_dough('water', 'flour') == 'Dough')
-print("Got:", make_dough('water', 'flour'))
+print("When make_dough is called with 'water' and 'flour' it should return 'dough'")
+print(make_dough('water', 'flour') == 'dough')
+print("Output:", make_dough('water', 'flour'))
 
 
-# Test 2
+# Test 2  -  make_dough()
 
 print("When make_dough is called with 'water' and 'cement' it should return 'not dough'")
-print(make_dough('water', 'cement') == 'Not dough')
-print("Got:", make_dough('water', 'cement'))
+print(make_dough('water', 'cement') == 'not dough')
+print("Output:", make_dough('water', 'cement'))
+
+# Test 3  -  bake_bread()
+
+print("When bake_bread is called with 'dough' it should return 'bread'")
+print(bake_bread('dough') == 'bread')
+print("Output:", bake_bread('dough'))
+
+# Test 4  -  bake_bread()
+
+print("When bake_bread is called with 'not dough' it should return 'not bread'")
+print(bake_bread('not dough') == 'not bread')
+print("Output:", bake_bread('not dough'))
+
+### PART 2 - Whole wheat dough / bread ###
+# Test 5  -  make_ww_dough()
+
+print("When make_ww_dough is called with 'water' and 'whole wheat flour' it should return 'whole wheat dough'")
+print(make_ww_dough('water', 'whole wheat flour') == 'whole wheat dough')
+print("Output:", make_ww_dough('water', 'whole wheat flour'))
+
+# Test 6  -  bake_ww_bread()
+
+print("When bake_ww_bread is called with 'whole wheat dough' it should return 'whole wheat bread'")
+print(bake_ww_bread('whole wheat dough') == 'whole wheat bread')
+print("Output:", bake_ww_bread('whole wheat dough'))
